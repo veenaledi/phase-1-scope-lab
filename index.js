e171b5c7a95212f5bed24286c345089b4e4452c3
neema <naledineema@gmail.com>
2024-07-13 11:49:56 +0300

    .results.json index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    console.log.upperCaseCustomerName('BOB')
+}
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    var bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'bob'
+function changeLeastFavoriteCustomer(){
+    var leastFavoriteCustomer = ''
+}
